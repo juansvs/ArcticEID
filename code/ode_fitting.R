6c17b5aa86dfdd88d02044203f1bab388d390732
juansvs <juans.vs@gmail.com>
2024-05-07 17:16:40 +0200

    editing ode fitting code for caribou data

--- code/ode_fitting.R
+++ code/ode_fitting.R
@@ -2,6 +2,7 @@
 #' ODE of an epidemiological model to actual data. 
 
 library(lubridate)
+library(tidyverse)
 library(dplyr)
 library(wbstats)
 library(deSolve)
@@ -11,8 +12,8 @@ library(parallel)
 # The RHS function for KMK
 RHS_KMK_SIR <- function(t, x, p) {
   with(as.list(c(x,p)), {
-    dS <- -beta*S*I/pop
-    dI <- beta*S*I/pop-gamma*I
+    dS <- -beta*S*I
+    dI <- beta*S*I-gamma*I
     dR <- gamma*I
     return(list(c(dS, dI, dR)))
   })
@@ -24,7 +25,7 @@ error_incidence <- function(p_vary,
                             params, 
                             incidence_data,
                             method = "rk4",
-                            which_incidence = "inc100") {
+                            which_incidence = "Prevalence") {
   # Anything that changes during optimisation needs to be set here
   params$beta = as.numeric(p_vary["beta"])
   params$gamma = as.numeric(p_vary["gamma"])
@@ -48,7 +49,7 @@ error_incidence <- function(p_vary,
     return(Inf)
   }
   # Values required to compute the error
-  incidence_from_run = params$beta * sol[,"S"] * sol[,"I"] / params$pop
+  incidence_from_run = sol[,"I"]/(sol[,'S']+sol[,"I"]+sol[,"R"])
   # Compute the error
   diff_values = incidence_data[[which_incidence]] - incidence_from_run
   diff_values_squared = diff_values^2
@@ -56,65 +57,30 @@ error_incidence <- function(p_vary,
   return(error)
 }
 
-# Read in the data for France
-data = readr::read_csv("https://www.sentiweb.fr/datasets/incidence-PAY-3.csv",
-                       skip = 1)
+# Read in the data
+data = readr::read_csv("../data/caribou_seroprevalence.csv")
 # Show the top of the table (always useful)
 head(data)
-# Week is by epi week, let's sort this out. Four first digits are year, last 
-# two are week. Let us rename the first column as yyyyww, to avoid confusion
-# with week
-colnames(data)[1] = "yyyyww" 
-data$year = substr(data$yyyyww, 1, 4)
-data$week = substr(data$yyyyww, 5, 6)
 # It will be easier to work with days as days, so we convert dates
-data$date = lubridate::parse_date_time(paste(data$year, data$week, 1, sep="/"),
-                                       'Y/W/w')
-# There are some issues, let us fix without thinking: if something went wrong, it 
-# is typically because of a week numbered 53. We make that day 31 December of 
-# that year
-idx = which(is.na(data$date))
-data$date[idx] = ymd(paste0(data$year[idx],"-12-31"))
+data$date = lubridate::parse_date_time(data$Month,'%B')
 
 # Ah, yes, don't forget: let's flip the order of the table, as it is sorted
 # by decreasing dates
-data = data %>%
-  arrange(date)
+data = data %>%  arrange(date)
 
 # Type of incidence to use (column name in the data)
-which_incidence = "inc"
+which_incidence = "Prevalence"
 
-# Let us now select one epidemic season (mid year to next). Use another variable
-# so we don't have to reload the data if we do weird edits..
-# We take a flu season prior to the pandemic (they are weird after that)
-beg_year = 2018
-end_year = 2019
-data_subset = data %>%
-  filter(date > paste0(beg_year,"-12-15")) %>%
-  filter(date <= ymd(paste0(end_year,"-04-30")))
+data_subset = filter(data, Total>8)
 
-plot(data_subset$date, data_subset[[which_incidence]],
-     type = "b",
-     xlab = "Month", ylab = "Incidence",
-     main = paste0(beg_year, "-", end_year, " flu season"))
+ggplot(data_subset,aes(format(date,"%m"),Prevalence))+geom_point(aes(color=Herd))
 
-# To do work, we will need the total population of France
-pop_data_FRA <- wb_data(country = "FRA", indicator = "SP.POP.TOTL",
-                        mrv = 100, return_wide = FALSE) %>%
-  filter(date == beg_year)
-# For numerical reasons, we might work per 100K people.. Detect this.
-if (grepl("100", which_incidence)) {
-  pop_data_FRA = pop_data_FRA$value / 100000
-} else {
-  pop_data_FRA = pop_data_FRA$value
-}
 
 params = list()
-params$gamma = 1/3.5   # Let's see if we can fit with this simple value
+params$gamma = 1/10   # Let's see if we can fit with this simple value
 # R0=beta/gamma, so beta=R0*gamma
-params$beta = 1.5*params$gamma
-# Add population data to this, for convenience
-params$pop = pop_data_FRA
+params$beta = 1
+params$pop = 0.5 # density of caribou
 
 ## Fit using a genetic algorithm
 GA = ga(
@@ -124,9 +90,9 @@ GA = ga(
                                          incidence_data = data_subset,
                                          which_incidence = which_incidence,
                                          method = "rk4"),
-  parallel = 120,
-  lower = c(0.1, 1/20),
-  upper = c(1, 1/2),
+  parallel = 4,
+  lower = c(1, 0.01),
+  upper = c(3, 0.1),
   pcrossover = 0.7,
   pmutation = 0.2,
   optim = TRUE,
@@ -138,7 +104,7 @@ GA = ga(
 
 params$beta = GA@solution[1]
 params$gamma = GA@solution[2]
-S0 = pop_data_FRA
+S0 = 0.2
 I0 = data_subset[[which_incidence]][1]/(params$beta)
 IC = c(S = S0, I = I0, R = 0)
 dates_num = as.numeric(as.Date(data_subset$date))
